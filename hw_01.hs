1ef3d3d4bab328b9adb665090067a50a322d7547
egorsmth <mattspring0@gmail.com>
2020-07-06 23:32:58 +0300

    v2

--- hw_01.hs
+++ hw_01.hs
@@ -1,19 +1,17 @@
-lineSolver :: Float -> Float -> [Float]
-lineSolver b c = 
-    if b == 0 
-    then [c] 
-    else [-c / b]
+lineSolver :: Float -> Float -> Float
+lineSolver b c 
+    | b == 0 = c
+    | otherwise = -c / b
 
 quadPolySolver :: Float -> Float -> Float -> [Float]
-quadPolySolver a b c = 
-    if a == 0 
-    then lineSolver b c 
-    else inner a b c
+quadPolySolver a b c
+    | a == 0 = [lineSolver b c]
+    | otherwise = inner a b c
     where 
         d = b*b - 4 * a * c
         inner a b c
             | d < 0     = []
-            | d == 0    = [-b / (2 * a)]
+            | d == 0    = [(-b + sqrt d) / (2 * a)]
             | otherwise = [(-b + sqrt d) / (2 * a), (-b - sqrt d) / (2 * a)]
             
         
@@ -25,9 +23,9 @@ fib n
 
 fibTailed :: Int -> Int
 fibTailed n = fibInner 0 1 n
-    where fibInner prev cur n = 
-        if n == 0 
-        then cur 
-        else fibInner cur (prev+cur) (n-1)
+    where 
+        fibInner prev cur n 
+            | n == 0 = cur 
+            | otherwise = fibInner cur (prev+cur) (n-1)
 
 main = print $ quadPolySolver 1 (-3) (-4)
